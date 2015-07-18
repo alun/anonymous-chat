5a397e4a5be941c089122842618b3e51a6eec7d1
Alexey Lunacharsky <alun@katlex.com>
2015-07-19 02:00:18 +0600

    Simple usable code for text chat

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject algoterm "1.0.0-SNAPSHOT"
+  :plugins [[katlex/github-cdn "0.1.0"]]
+  :github-cdn {:dir "site"
+               :repository "git@github.com:alun/anonymous-chat"
+               :branch "gh-pages"})
